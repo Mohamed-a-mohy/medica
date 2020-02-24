cf36f1d5b8928f1888efda3867a6352f8ff563e6
mohamed.amohy97@gmail.com <mohamed.amohy97@gmail.com>
2020-02-24 15:00:59 +0200

    style added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddtocartService } from '../addtocart.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+arrOfData = []
+category = []
+
+
+
+  constructor(private service: AddtocartService) {
+    this.service.getData.subscribe(items => {
+      this.arrOfData = items;
+      console.log(this.arrOfData);
+      if(this.arrOfData[0]){
+        for (let i = 0; i < this.arrOfData.length; i++) {
+          if (this.category[0]) {
+            for (let j = 0; j < this.category.length; j++) {
+              if (this.category.indexOf(this.arrOfData[i].category) == -1 && this.arrOfData[i].category != undefined) {
+                this.category.push(this.arrOfData[i].category)
+              }
+            }
+          } else {
+            this.category.push(this.arrOfData[i].category)
+          }
+        }
+        console.log(this.arrOfData);
+        console.log(this.category);
+      }})
+  }
  
 
   ngOnInit() {
     
+      
+      
   }
 
 }
